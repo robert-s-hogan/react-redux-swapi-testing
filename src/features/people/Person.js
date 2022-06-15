a1556d0fbd6c4c11f17e64e06eab494b52d46b4c
Robert Hogan <rhogan7@gmail.com>
2022-06-15 05:45:30 -0700

    Added characters styling from previous project.

--- src/features/people/Person.js
+++ src/features/people/Person.js
@@ -1,8 +1,19 @@
-const Character = ({ name, homeworld }) => {
+import CardFace from './peopleComponents/CardFace';
+
+const Character = ({ name, hair_color, eye_color, gender, skin_color, homeworld }) => {
   return (
-    <>
+    <div className={`card rounded-lg relative overflow-hidden h-full`}>
       <p className="text-white font-semibold uppercase text-xl">{name}</p>
-    </>
+      <CardFace
+        name={name}
+        hairColor={hair_color}
+        eyeColor={eye_color}
+        gender={gender}
+        skinColor={skin_color}
+        // homeworld={homeworldName}
+        styles="card-face absolute bottom-36 -right-28 mr-1 mb-9 z-10"
+      />
+    </div>
   );
 };
 
