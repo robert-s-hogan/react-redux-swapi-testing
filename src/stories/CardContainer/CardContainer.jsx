82b177ec1773cc9f1322da46677152cde2acf391
Robert Hogan <rhogan7@gmail.com>
2022-06-22 07:05:57 -0700

    Added storybook

---
+++ src/stories/CardContainer/CardContainer.jsx
@@ -0,0 +1,6 @@
+import PropTypes from 'prop-types';
+
+export const CardContainer = ({ children }) => {
+  return <div className="letter-box h-96">{children}</div>;
+};
+export default CardContainer;
